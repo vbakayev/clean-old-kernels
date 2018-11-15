240bcbac7bb03ba93d9ecc60d46d9b50fed34f38
Vladislav Bakayev <vbakayev@gmail.com>
2018-11-15 14:41:52 +0100

    Couple of versions further, changes:
     - not ask any questions - with "-y" option (version 1.1)
     - run as cronjob, producing no output (version 1.2)
     - cleaner flow, less external calls, small bugfix (1.3)

--- clean-old-kernels.sh
+++ clean-old-kernels.sh
@@ -1,84 +1,73 @@
 #!/bin/bash
 
-# Fixed and adapted from:
-# http://stoilis.wordpress.com/2010/06/18/automatically-remove-old-kernels-from-debian-based-distributions/
-# by Julien Moreau AKA PixEye.
+# adapted from:
+#  http://stoilis.wordpress.com/2010/06/18/automatically-remove-old-kernels-from-debian-based-distributions/
+#  by Julien Moreau AKA PixEye.
+# reworked by vbakayev to be able to:
+# - not ask any questions - with "-y" option (version 1.1)
+# - run as cronjob, producing no output (version 1.2)
+# - cleaner flow, less external calls, small bugfix (1.3)
+
+echo_if_tty () { if [[ -t 1 ]]; then echo "$@"; fi }
 
 # Text color variables
-bold=$(tput bold)
 rst=$(tput sgr0)	# Reset
 red=$(tput setaf 1)
 green=$(tput setaf 2)
 yellow=$(tput setaf 3)
-# options:
-underline=$(tput sgr 0 1)
 blue=$(tput setaf 4)
 purple=$(tput setaf 5)
 cyan=$(tput setaf 6)
 white=$(tput setaf 7)
+# options:
+bold=$(tput bold)
+underline=$(tput sgr 0 1)
 
-cd /boot || exit $?
-INSTALLED_KERNELS=`ls -1 vm*`
-NUMBER_OF_INSTALLED_KERNELS=`echo $INSTALLED_KERNELS |wc -w`
-LAST_INSTALLED_KERNEL=`ls -1 vm* |tail -1`
-CURRENT_VERSION=`uname -r`
-RUNNING_KERNEL=`ls vm*$CURRENT_VERSION`
-if test "$RUNNING_KERNEL" = "$LAST_INSTALLED_KERNEL"
-then addon='' ; else addon=$red
+pushd /boot &>/dev/null || exit $?
+INSTALLED_KERNELS=$(ls --color=never -1r vmlinuz-*)
+NUMBER_OF_INSTALLED_KERNELS=$(echo ${INSTALLED_KERNELS} | wc -w)
+NUMBER_OF_KERNELS_TO_KEEP=2
+LAST_INSTALLED_KERNEL=$(ls --color=never -1 vmlinuz-* | tail -n 1)
+CURRENT_VERSION=$(uname -r)
+RUNNING_KERNEL=$(ls --color=never -1 vmlinuz-$(uname -r))
+popd &>/dev/null
+
+if [ "${RUNNING_KERNEL}" != "${LAST_INSTALLED_KERNEL}" ]; then
+  a1=${red}; a2=${green}
+  echo_if_tty -e "${bold}${yellow}You need to reboot with the last installed kernel.${rst}"
 fi
+echo_if_tty -e "${bold}Current kernel is.......: ${a1}${RUNNING_KERNEL}${rst}"
+echo_if_tty "/ Installed kernels:"
+echo_if_tty ${INSTALLED_KERNELS}
+echo_if_tty "\ -> ${NUMBER_OF_INSTALLED_KERNELS} kernels installed."
+echo_if_tty -e "${bold}Last installed kernel is: ${a2}${LAST_INSTALLED_KERNEL}${rst}"
 
-echo -e "${bold}Current kernel is.......: $addon$RUNNING_KERNEL$rst"
-
-echo "/ Installed kernels:"
-echo "$INSTALLED_KERNELS"|nl
-echo "\ -> $NUMBER_OF_INSTALLED_KERNELS kernels installed."
-
-if test "$RUNNING_KERNEL" = "$LAST_INSTALLED_KERNEL"
-then addon='' ; else addon=$green
-fi
-echo -e "${bold}Last installed kernel is: $addon$LAST_INSTALLED_KERNEL$rst"
-
-i=1
-KERNELS_TO_REMOVE=''
-for k in $INSTALLED_KERNELS
+i=0; REMOVE_PATTERN=''
+for k in ${INSTALLED_KERNELS}
 do
-	k="`echo $k |cut -d'-' -f2-`"
-	# echo "k='$k'"	# for debug
+  let $((i++))
+  if [ ${i} -le ${NUMBER_OF_KERNELS_TO_KEEP} ]; then continue ; fi  # keep at least that many kernels
+  if [ "${k}" == "${RUNNING_KERNEL}" ]; then continue; fi # do not remove running kernel
 
-	if [ "vmlinuz-$k" == "$RUNNING_KERNEL" ]
-	then continue
-	# else echo "$k != $RUNNING_KERNEL"	# for debug
-	fi
+  k="$(echo ${k##vmlinuz-} | cut -d- -f-2)"
+  # echo_if_tty "k='$k'"  # for debug
 
-	i=`expr $i + 1`
-	if [ $i -ge $NUMBER_OF_INSTALLED_KERNELS ] ; then continue ; fi	# keep a backup kernel
-
-	k="`echo $k |awk -F- '{print $1"-"$2}'`"
-	if [ "$KERNELS_TO_REMOVE" == '' ]
-	then KERNELS_TO_REMOVE="$k"
-	else KERNELS_TO_REMOVE="$KERNELS_TO_REMOVE|$k"
-	fi
+  if [ "${REMOVE_PATTERN}" == '' ]; then
+    REMOVE_PATTERN="${k}"
+  else
+    REMOVE_PATTERN="${REMOVE_PATTERN}|${k}"
+  fi
 done
 
-if test "$RUNNING_KERNEL" != "$LAST_INSTALLED_KERNEL"
-then echo "$bold${yellow}You need to reboot with the last installed kernel.$rst"
+if [ "${REMOVE_PATTERN}" == '' ]; then
+  echo_if_tty -e "${bold}${green}There is no kernel to remove.${rst}" ; exit 0
 fi
 
-if [ "$KERNELS_TO_REMOVE" == '' ]
-then echo -e "$bold${green}There is no kernel to remove.$rst" ; exit 1
+echo_if_tty -e "${bold}${yellow}Kernels to remove (pattern): ${REMOVE_PATTERN}.${rst}"
+
+if [ "${LOGNAME}" != "root" ]; then
+  echo_if_tty -e "${bold}${red}I need root privileges in order to clean!${rst}" ; exit 2
 fi
 
-echo -e "$bold${yellow}Kernels to remove: ${KERNELS_TO_REMOVE}.$rst"
-
-#if test "$RUNNING_KERNEL" != "$LAST_INSTALLED_KERNEL"
-#then
-#	echo "$bold${red}Firstly, you need to reboot with the last installed kernel please!$rst"
-#	exit 1
-#fi
-
-if test `whoami` != 'root'
-then echo -e "$bold${red}I need root privileges in order to clean!$rst" ; exit 2
-fi
-
-# set -x # Set verbose mode
-apt-get remove --purge `dpkg -l |awk '{print $2}' |egrep "$KERNELS_TO_REMOVE"`
+REMOVE_PACKAGES=$(dpkg -l linux-\* | awk "/${REMOVE_PATTERN}/ {print \$2}")
+apt-get purge ${1} ${REMOVE_PACKAGES}
